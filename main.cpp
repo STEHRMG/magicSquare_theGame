70c79e697596ecdb94a9f6470391822a1cac5189
Štěpán Řezníček <rezniste@fit.cvut.cz>
2025-04-29 22:45:22 +0200

    Add position finding

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 #include <array>
 #include <cstdlib>
+#include <cassert>
+
+struct Position
+{
+	std::size_t row{ 0 };
+	std::size_t col{ 0 };
+};
+
+template <typename T, std::size_t Col, std::size_t Row>
+
+Position findPosition(const std::array<std::array<T, Col>, Row>& array2D)
+{
+	Position position{};
+
+        for(const auto& arrRow: array2D)
+        {
+                for(const auto& arrCol: arrRow)
+                {
+                       	if(arrCol == 0)
+				return position;
+			++position.col;
+                }
+		position.col = 0;
+                ++position.row;
+        }
+}
 
 template <typename T, std::size_t Col, std::size_t Row>
 
@@ -16,15 +42,27 @@ void printArray(const std::array<std::array<T, Col>, Row>& array2D)
 	}
 }
 
+template <typename T, std::size_t Col, std::size_t Row>
+
+void moveTile(std::array<std::array<T, Col>, Row>& array2D)
+{
+	Position position{ findPosition(array2D) };
+	//assert(position.row == 0 && position.col == 0);
+
+	std::cout << position.row << '\t' << position.col << '\n';
+}
+
 int main()
 {
 	std::array<std::array<int, 3>, 3> gameBoard{{
-		{0, 1, 2},
+		{8, 1, 2},
 	       	{3, 4, 5},
-	       	{6, 7, 8}
+	       	{6, 7, 0}
 	}};
 
 	printArray(gameBoard);
+	moveTile(gameBoard);
+
 
 	return EXIT_SUCCESS;
 }
