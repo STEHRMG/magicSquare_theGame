5e2e3f7fee97c7a5b779f2b6a1a18531cc00b6af
Štěpán Řezníček <rezniste@fit.cvut.cz>
2025-04-29 22:12:33 +0200

    Add gameboard printing

--- main.cpp
+++ main.cpp
@@ -2,13 +2,29 @@
 #include <array>
 #include <cstdlib>
 
+template <typename T, std::size_t Col, std::size_t Row>
+
+void printArray(const std::array<std::array<T, Col>, Row>& array2D)
+{
+	for(const auto& arrRow: array2D)
+	{
+		for(const auto& arrCol: arrRow)
+		{
+			std::cout << arrCol << '\t';
+		}
+		std::cout << '\n';
+	}
+}
+
 int main()
 {
-	[[maybe_unused]] std::array<std::array<int, 3>, 3> gameBoard{{
+	std::array<std::array<int, 3>, 3> gameBoard{{
 		{0, 1, 2},
 	       	{3, 4, 5},
 	       	{6, 7, 8}
 	}};
+
+	printArray(gameBoard);
 
 	return EXIT_SUCCESS;
 }
